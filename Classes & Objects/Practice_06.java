4c5a1bf2f417ff37a4d3a65daff1bf43c18be738
ponrajfaceprep <155868895+ponrajfaceprep@users.noreply.github.com>
2024-12-13 10:34:35 +0530

    Update Practice_06.java

--- Classes & Objects/Practice_06.java
+++ Classes & Objects/Practice_06.java
@@ -1,39 +1,39 @@
 
-//class Student {
-//	// Student details
-//	int studentId;
-//	String studentName;
-//	String studentDept;
-//	
-//	// Constructor
-//	Student(int sId, String sName, String sDept){
-//		studentId = sId;
-//		studentName = sName;
-//		studentDept = sDept;
-//	}
-//
-//	// Printing Student details
-//	public void display() {
-//		System.out.println("Student Id : " + studentId);
-//		System.out.println("Student Name : " + studentName);
-//		System.out.println("Student Dept : " + studentDept + "\n");
-//	}
-//}
+class Student {
+	// Student details
+	int studentId;
+	String studentName;
+	String studentDept;
+	
+	// Constructor
+	Student(int sId, String sName, String sDept){
+		studentId = sId;
+		studentName = sName;
+		studentDept = sDept;
+	}
 
-//public class Practice_06 {
-//	public static void main(String[] args) {
-//
-//		Student s1 = new Student(1001, "Ponraj", "ECE");
-//		Student s2 = new Student(1002, "Subhiksha", "CSE");
-//		Student s3 = new Student(1003, "Kavya", "IT");
-//
-//		// Printing Student 1 details
-//		s1.display();
-//
-//		// Printing Student 2 details
-//		s2.display();
-//
-//		// Printing Student 2 details
-//		s3.display();
-//	}
-//}
+	// Printing Student details
+	public void display() {
+		System.out.println("Student Id : " + studentId);
+		System.out.println("Student Name : " + studentName);
+		System.out.println("Student Dept : " + studentDept + "\n");
+	}
+}
+
+public class Practice_06 {
+	public static void main(String[] args) {
+
+		Student s1 = new Student(1001, "Ponraj", "ECE");
+		Student s2 = new Student(1002, "Subhiksha", "CSE");
+		Student s3 = new Student(1003, "Kavya", "IT");
+
+		// Printing Student 1 details
+		s1.display();
+
+		// Printing Student 2 details
+		s2.display();
+
+		// Printing Student 2 details
+		s3.display();
+	}
+}
